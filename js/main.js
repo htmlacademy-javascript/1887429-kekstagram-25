8e631a4278904f15c0708cfc08c450ce18cddd95
Глебас <strosher@gmail.com>
2022-04-16 08:50:37 +0300

    Готово

--- js/main.js
+++ js/main.js
@@ -1,2 +1,14 @@
-import './miniatures.js';
+import './util.js';
+import {fillMainPage} from './miniature.js';
+import {sortMiniatures} from './filter-miniatures.js';
+import './const.js';
+import './modal.js';
+import {closeFormEdit, setUserFormSubmit} from './user-form.js';
+import {getData} from './api.js';
 
+getData((pictures)=> {
+  fillMainPage(pictures);
+  sortMiniatures(pictures);
+});
+
+setUserFormSubmit(closeFormEdit);
